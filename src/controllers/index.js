3f4b23d786bafb0dd3ec03b2cb948a9fd7d4f3d4
RosareMililani <mr7721@rit.edu>
2020-10-15 19:00:34 -0400

    updated/finished Express Demo

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -16,13 +16,34 @@ const hostPage2 = (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/../../views/page2.html`));
 };
 
-const notFound = (req,res) =>{
+const notFound = (req, res) => {
   res.status(404).sendFile(path.resolve(`${__dirname}/../../views/notFound.html`));
 }
+
+const getName = (req, res) => {
+  res.json({ name });
+}
+
+const setName = (req, res) => {
+  console.dir(req.body);
+
+  // handle the error condition first
+  if (!req.body.firstname || !req.body.lastname) {
+    return res.status(400).json({ error: "firstname and lastname are both required", id: "badRequest" });
+  }
+
+  // if the required params are present, update `name` and send it back
+  name = `${req.body.firstname} ${req.body.lastname}`;
+  // return res.status(204).json({name});
+  return res.json({ name });
+}
+
 
 module.exports = {
   index: hostIndex,
   page1: hostPage1,
   page2: hostPage2,
-  notFound: notFound
+  notFound,
+  getName,
+  setName,
 }
--- src/router.js
+++ src/router.js
@@ -5,7 +5,10 @@ const router = (app) => {
     app.get('/', controllers.index);
     app.get('/page1', controllers.page1);
     app.get('/page2', controllers.page2);
+    app.get('/getName', controllers.getName);
     app.get('/*',controllers.notFound); // order matters!
+
+    app.post('/setName', controllers.setName);
 };
 
 // export the router function
